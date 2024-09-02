ebdff7df0a92c5512b954f7edb543e362af3ecfa
türkay ürkmez <turkay.urkmez@outlook.com>
2024-09-02 13:59:49 +0300

    using Controller and action

---
+++ IntroduceDotnetCore/IntroduceDotnetCore/Controllers/CemilController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntroduceDotnetCore.Controllers
+{
+    public class CemilController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Orhan(string? test, string? ad)
+        {
+            //Amaç: 
+            dynamic value = "tarım";
+            var result = value.ToUpper();
+            ViewBag.Deger = test;
+            ViewBag.Ad = ad;
+            return View();
+        }
+    }
+}
--- IntroduceDotnetCore/IntroduceDotnetCore/Program.cs
+++ IntroduceDotnetCore/IntroduceDotnetCore/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddControllersWithViews();
 
-app.MapGet("/", () => "Hello World!");
+
+var app = builder.Build();
+//Ctrl K+C
+app.UseRouting();
+
+app.MapControllerRoute(name: "default",
+                       pattern: "{controller=Cemil}/{action=Orhan}/{ad?}");
 
 app.Run();
+//Ctrl+shift+B
