e9ce63017df4400361adeffaafc6415dd2433a77
türkay ürkmez <turkay.urkmez@outlook.com>
2024-09-06 12:02:56 +0300

    created API

---
+++ minimalAPI/minimalAPI/Data/TasksDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace minimalAPI.Data
+{
+    public class TasksDbContext : DbContext
+    {
+        public TasksDbContext(DbContextOptions<TasksDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Todo> Todos { get; set; }
+
+
+    }
+}
